4ecb751589d52df51d056043eeb450ffd1d1cbf4
Sean Nam <blt24q4@gmail.com>
2024-12-09 16:56:00 +0900

    Update 03-strings.js

--- 03-strings.js
+++ 03-strings.js
@@ -13,3 +13,60 @@ console.log(s1.substr(7, 10));  //  인덱스 7부터 10개
 //  -> deprecated (폐지 예정)
 console.log(s1.substring(7, 17)); //  인덱스 7부터 17 앞까지
 console.log(s1.substring(7));  //   인덱스 7부터 끝까지
+
+//  문자열 검색 메서드
+console.log("원본:", s1);
+
+let pos = s1.indexOf("Java");
+console.log("1st Search:", pos); // 첫 번째 Java의 위치
+pos = s1.indexOf("Java", pos + 4);  //  검색 시작위치 재정의
+console.log("2nd Search:", pos); // 두 번째 Java의 위치
+pos = s1.indexOf("Java", pos + 4);  //  검색어를 찾지 못함
+console.log("No Keyword:", pos);    //  -1 : 없음
+
+pos = s1.lastIndexOf("Script"); //  뒤로부터 앞으로 검색
+console.log("lastIndexOf:", pos);
+
+//  문차열 치환
+console.log("원본:", s1);
+console.log("replace:", s1.replace("JavaScript",
+                                    "ECMAScript"));
+console.log("원본:", s1);                                    
+
+//  좌위 화이트 스페이스 제거
+const noisyStr = "          Hello, ECMAScript       ";
+console.log(noisyStr);
+console.log(noisyStr.trim());
+
+//  문자열 보충 수업
+//  이스케이프 문자 -> 매우 유의
+//  \n : 줄바꿈
+//  \t : 탭
+//  \' : 작은 따옴표
+//  \" : 큰 따옴표
+//  \` : 백틱(ES6)
+//  \$ : 달러 기호(ES6)
+
+let message = 'He said, "Hello"';
+console.log(message);
+message = "He said, \"Hello\"";
+
+// 현재 실내 온도는 __ 도 입니다.
+let temp = 23;
+message = "현재 실내 온도는 " + temp + "도 입니다.";
+console.log(message);
+
+//  템플릿 문자열: 백틱(`), ${}
+message = `현재 실내 온도는 ${temp} 도 입니다.`;
+console.log(message);
+
+//  여러 줄 문자열도 템플릿 문자열로 쉽게 표현 가능
+message =
+"예전에는 여러 줄\n문자열을 만드는 게\n쉽지 않았습니다.";
+console.log(message);
+
+message = `
+하지만 ES6에서는
+    아주 손쉽게
+    여러 줄 문자열을 만들 수 있습니다.`;
+console.log(message);
